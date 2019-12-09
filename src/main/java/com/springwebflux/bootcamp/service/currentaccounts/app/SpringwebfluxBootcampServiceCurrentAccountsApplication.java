d0c747c5e2558c5fe96f56f1225b8341fd560f59
hernanspc <iphone1198@icloud.com>
2019-12-09 10:11:56 -0500

    first commit

---
+++ src/main/java/com/springwebflux/bootcamp/service/currentaccounts/app/SpringwebfluxBootcampServiceCurrentAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.springwebflux.bootcamp.service.currentaccounts.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxBootcampServiceCurrentAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebfluxBootcampServiceCurrentAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springwebflux/bootcamp/service/currentaccounts/app/SpringwebfluxBootcampServiceCurrentAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebflux.bootcamp.service.currentaccounts.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebfluxBootcampServiceCurrentAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
